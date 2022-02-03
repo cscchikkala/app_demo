6f34cd2c312a1cec98d5f6ce9a52bbf05c474ef6
cscchikkala <chaitanya.chikkala@gmail.com>
2022-02-03 15:17:06 +0000

    Delete app_demo-main/app directory

--- app_demo-main/app/backends.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-dev-rg"
-    storage_account_name = "kktfstatedevstg"
-    container_name       = "akstfstatefiles"
-    key                  = "appservice.tfstate"
-  }
-}
